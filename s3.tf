994c79fbef1efeba2958bf5d56ecb29166fe18c6
Fionn Masuhr <git@fionn.io>
2022-02-14 08:53:01 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -15,12 +15,12 @@ resource "aws_s3_bucket_acl" "this" {
 
   access_control_policy {
     owner {
-      id = data.aws_canonical_user_id.current.bucket
+      id = data.aws_canonical_user_id.current.id
     }
 
     grant {
       grantee {
-        id   = data.aws_canonical_user_id.current.bucket
+        id   = data.aws_canonical_user_id.current.id
         type = "CanonicalUser"
       }
       permission = "FULL_CONTROL"
