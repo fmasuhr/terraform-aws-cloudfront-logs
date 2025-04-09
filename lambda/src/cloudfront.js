81b0ee6dbf91b85ed26f5cfdc24c54a68f56bd39
Fionn Masuhr <git@fionn.io>
2025-04-09 15:54:36 +0200

    Drop unused eslint configuration

--- lambda/src/cloudfront.js
+++ lambda/src/cloudfront.js
@@ -20,7 +20,6 @@ const parseVersion = (line) => {
 // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html#LogFileFormat
 //
 // Format:
-// eslint-disable-next-line max-len
 //   #Fields: date time x-edge-location sc-bytes c-ip cs-method cs(Host) cs-uri-stem sc-status cs(Referer) cs(User-Agent) cs-uri-query cs(Cookie) x-edge-result-type x-edge-request-id x-host-header cs-protocol cs-bytes time-taken x-forwarded-for ssl-protocol ssl-cipher x-edge-response-result-type cs-protocol-version fle-status fle-encrypted-fields
 //
 const parseFields = (line) => {
